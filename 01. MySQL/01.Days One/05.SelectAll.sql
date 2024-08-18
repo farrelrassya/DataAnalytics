0a2b755b7c9150b15739915dfe951c2da98b86bb
Farrel Rassya <91480200+farrelrassya@users.noreply.github.com>
2024-08-18 16:08:49 +0700

    Add files via upload

---
+++ 01. MySQL/01.Days One/05.SelectAll.sql
@@ -0,0 +1,13 @@
+/*
+This query retrieves all columns from the EmployeeDemographics/EmployeeSalary table.
+The purpose of this query is to provide a full view of the demographic data 
+of all employees within the organization.
+
+The asterisk (*) is used as a wildcard to select all available columns.
+
+*/
+SELECT * 
+FROM EmployeeDemographics;
+
+SELECT *
+FROM EmployeeSalary
